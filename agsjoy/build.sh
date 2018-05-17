c915fa7fd6aa3d0d151f8dfc0192d333cc8993cf
ericoporto <ericoporto2008@gmail.com>
2018-05-17 01:09:20 -0300

    skip SDL_pollevent if using ags-sdl2 port, instead filter for joystick events!

--- agsjoy/build.sh
+++ agsjoy/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #tested in ubuntu 16.04
 
-g++ -shared -o libagsjoy.so -fPIC  agsjoy.cpp `sdl2-config --cflags --libs`
+g++ -std=c++0x -shared -o libagsjoy.so -fPIC  agsjoy.cpp `sdl2-config --cflags --libs`
 #g++ -lSDL2 -shared -o libagsjoy32.so -fPIC -m32 agsjoy.cpp 
